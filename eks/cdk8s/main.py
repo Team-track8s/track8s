56db2ee001b4b033089133d443f69c14444d5f54
fraisai <r.uddin292@gmail.com>
2022-12-04 16:44:25 -0500

    applied service and deployment manifests

--- eks/cdk8s/main.py
+++ eks/cdk8s/main.py
@@ -1,12 +1,43 @@
 #!/usr/bin/env python
 from constructs import Construct
 from cdk8s import App, Chart
+from imports import k8s
 
+# super() is called 2x because it creates the YAML file with the name specified and appends any config generated after that point by calling the parent class constructor that this class inherits from
 class MyChart(Chart):
     def __init__(self, scope: Construct, id: str):
-        super().__init__(scope, id)
+        super().__init__(scope, id) # generates a file for the service config
 
-        # define resources here
+        # Label that will be used to tag pods
+        label = {"app": "cdk8s"}
+
+        k8s.KubeDeployment(
+            self, 
+            'deployment',
+            spec = k8s.DeploymentSpec(
+                replicas = 2,
+                selector = k8s.LabelSelector(match_labels = label),
+                template = k8s.PodTemplateSpec(
+                    metadata = k8s.ObjectMeta(labels = label),
+                    spec = k8s.PodSpec(containers = [
+                        k8s.Container(
+                            name = 'cdk8s',
+                            image = 'public.ecr.aws/s9u7u6x1/sample_app_001:no-db',
+                            ports = [k8s.ContainerPort(container_port = 80)]
+                        )
+                    ])
+                )
+            )
+        )
+
+        # Create the service to expose the pods to traffic from teh load balancer
+        super().__init__(scope, f"{id}-service") # generates a 2nd file for the deployment
+
+        k8s.KubeService(self, 'service',
+            spec = k8s.ServiceSpec(
+            type = 'LoadBalancer',
+            ports = [k8s.ServicePort(port = 80, target_port = k8s.IntOrString.from_number(80))],
+            selector = label))
 
 app = App()
 MyChart(app, "cdk8s")
--- eks/cluster/cluster/cluster_stack.py
+++ eks/cluster/cluster/cluster_stack.py
@@ -4,15 +4,13 @@ from aws_cdk import (
     aws_eks as eks,
     aws_iam as iam
 )
-
 from constructs import Construct
+import yaml
 
 class ClusterStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
 
         # Look up the default VPC
         vpc = ec2.Vpc.from_lookup(self, id="VPC", is_default=True)
@@ -20,9 +18,9 @@ class ClusterStack(Stack):
         # Create the master role for EKS Cluster
         iam_role = iam.Role(self, id=f"{construct_id}-iam", role_name=f"{construct_id}-am", assumed_by=iam.AccountRootPrincipal())
 
-        # Creating cluster with EKS
-        eks_cluster = eks.Cluster(
-            self, id=f"{construct_id}-cluster",
+        # Creating the Kubernetes cluster with EKS
+        eks_cluster = eks.Cluster(self, 
+            id=f"{construct_id}-cluster",
             cluster_name=f"{construct_id}-cluster",
             vpc=vpc,
             vpc_subnets=vpc.public_subnets,
@@ -30,3 +28,15 @@ class ClusterStack(Stack):
             default_capacity_instance=ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO), 
             version=eks.KubernetesVersion.V1_20
         )
+
+        # Read deployment config YAML file and create deployment_yaml variable to pass to the cluster
+        with open("../cdk8s/dist/cdk8s.k8s.yaml", 'r') as stream: 
+            deployment_yaml = yaml.load(stream, Loader=yaml.FullLoader)
+
+        # Read service config YAML file and create service_yaml variable to pass to the cluster
+        with open("../cdk8s/dist/cdk8s-service.k8s.yaml") as stream: 
+            service_yaml = yaml.load(stream, Loader=yaml.FullLoader)
+
+        # Pass the YAML files to the cluster using the add_manifest method to apply the service and deployment Kubernetes manifests to the cluster
+        eks_cluster.add_manifest(f"{construct_id}-app-deployment", deployment_yaml)
+        eks_cluster.add_manifest(f"{construct_id}-app-service", service_yaml)
