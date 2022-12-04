4e2ef9252e82983336062f728637a9909d5a6917
fraisai <r.uddin292@gmail.com>
2022-12-04 00:10:22 -0500

    configuring deployment and service

--- eks/cdk8s/main.py
+++ eks/cdk8s/main.py
@@ -2,13 +2,11 @@
 from constructs import Construct
 from cdk8s import App, Chart
 
-
 class MyChart(Chart):
     def __init__(self, scope: Construct, id: str):
         super().__init__(scope, id)
 
         # define resources here
-
 
 app = App()
 MyChart(app, "cdk8s")
